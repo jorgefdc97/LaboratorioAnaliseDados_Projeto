519e2eb40c58b24038cae7aa074f11fd57aeb4e6
Tbrandao23 <tiagoleal76@gmail.com>
2024-03-28 16:49:43 +0000

    Tentativa de fazer algo

--- data.py
+++ data.py
@@ -1,9 +1,22 @@
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv("GOOG.US_D1.csv")
+df = pd.read_csv("GOOG.US_D1_cleaned.csv")
 
-print(df)
+print('Description:\n',df.describe())
+
+
+
+stoch = df.loc[:,'open':'volume'] 
+rsi = df.loc[:,'rsi_3':'rsi_90']
+
+
+df_corr = pd.concat([stoch,rsi], axis=1)
+corr = df_corr[df_corr['volume'] > 50000000]
+
+sns.heatmap(corr.corr())
+plt.show() 
 
 
