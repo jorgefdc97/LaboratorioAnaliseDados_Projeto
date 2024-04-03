97291c25700d20b948f44bb13c44ad5d2f546bf6
Tbrandao23 <tiagoleal76@gmail.com>
2024-04-03 10:00:06 +0100

    alter

--- data.py
+++ data.py
@@ -5,17 +5,13 @@ import seaborn as sns
 
 df = pd.read_csv("GOOG.US_D1_cleaned.csv")
 
-print('Description:\n',df.describe())
+
 
 
 
 stoch = df.loc[:,'open':'volume'] 
-rsi = df.loc[:,'rsi_3':'rsi_90']
 
-
-df_corr = pd.concat([stoch,rsi], axis=1)
-corr = df_corr[df_corr['volume'] > 50000000]
-
+print('Description:\n',stoch.describe())
 sns.heatmap(corr.corr())
 plt.show() 
 
