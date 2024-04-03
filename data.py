e23e9e4ba84810a7dbf17f6b080d3f4e9dfa9145
Tbrandao23 <tiagoleal76@gmail.com>
2024-04-03 10:05:40 +0100

    cov and corr

--- data.py
+++ data.py
@@ -12,6 +12,9 @@ df = pd.read_csv("GOOG.US_D1_cleaned.csv")
 stoch = df.loc[:,'open':'volume'] 
 
 print('Description:\n',stoch.describe())
+print('Cocariance:\n',stoch.cov())
+print('Correlation:\n',stoch.corr())
+
 sns.heatmap(stoch.corr())
 plt.show() 
 
