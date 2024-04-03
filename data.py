3089702db498ffe7c9eaeb0adc531eb7d3ae4048
Tbrandao23 <tiagoleal76@gmail.com>
2024-04-03 10:15:01 +0100

    graphs

--- data.py
+++ data.py
@@ -5,15 +5,17 @@ import seaborn as sns
 
 df = pd.read_csv("GOOG.US_D1_cleaned.csv")
 
-
-
-
-
 stoch = df.loc[:,'open':'volume'] 
 
 print('Description:\n',stoch.describe())
 print('Cocariance:\n',stoch.cov())
 print('Correlation:\n',stoch.corr())
+
+plt.scatter(stoch['open'],stoch['volume'])
+plt.show() 
+
+plt.bar(stoch['open'],stoch['volume'])
+plt.show() 
 
 sns.heatmap(stoch.corr())
 plt.show() 
