fe8c5d964293c9145fcc3bfc2f55df1a4ee2078c
Tbrandao23 <tiagoleal76@gmail.com>
2024-04-03 10:01:00 +0100

    altera

--- data.py
+++ data.py
@@ -12,7 +12,7 @@ df = pd.read_csv("GOOG.US_D1_cleaned.csv")
 stoch = df.loc[:,'open':'volume'] 
 
 print('Description:\n',stoch.describe())
-sns.heatmap(corr.corr())
+sns.heatmap(stoch.corr())
 plt.show() 
 
 
